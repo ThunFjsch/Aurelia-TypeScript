c7b94dc944ede29363a6815718518e102b840c6d
ThunFjsch <arinartz4@gmail.com>
2025-07-29 16:34:29 +0200

    relocated global definition to make main more thin

--- src/main.ts
+++ src/main.ts
@@ -3,94 +3,9 @@ import { Stats } from './stats'
 import { MemoryService } from "services/memory.service";
 import { logger } from "utils/logger/logger";
 import memHack from "./utils/memhack";
-import { envManager } from './utils/logger/envManager';
 import profiler, { Profiler } from "./utils/profiler/screeps-profiler"
 import { Planner } from "roomManager/basePlanner/planner";
-
-global.envManager = envManager;
-global.logger = logger;
-
-declare global {
-  /*
-    Example types, expand on these or remove them and add your own.
-    Note: Values, properties defined here do no fully *exist* by this type definiton alone.
-          You must also give them an implemention if you would like to use them. (ex. actually setting a `role` property in a Creeps memory)
-
-    Types added in this `global` block are in an ambient, global context. This is needed because `main.ts` is a module file (uses import or export).
-    Interfaces matching on name from @types/screeps will be merged. This is how you can extend the 'built-in' interfaces from @types/screeps.
-  */
-  // Memory extension samples
-  interface Memory {
-    uuid: number;
-    log: any;
-    myRooms: string[];
-    globalReset: number;
-    respawn?: boolean;
-    sourceInfo: SourceInfo[];
-    logs: any[];
-    env: string;
-    profiler?: ProfilerMemory;
-  }
-
-  interface SourceInfo {
-    my?: boolean;
-    id: Id<Source>;
-    pos?: RoomPosition;
-    spots: number;
-    energy: number;
-    ePerTick: number;
-    container: RoomPosition;
-    maxIncome: number;
-    maxWorkParts: number;
-    maxHaulerParts: number;
-    distance?: number;
-  }
-
-  interface Spot {
-    pos: RoomPosition;
-    path: RoomPosition[];
-    pathCost: number
-  }
-
-  interface RoomMemory {
-    isOwned: boolean;
-    remotes: string[];
-    hasRoads?: boolean;
-    basePlanner:BasePlanner
-  }
-
-  interface BasePlanner {
-    startlocation?: {x: number, y: number, score: number}
-    distanceTransform?: number[][]
-  }
-
-  interface CreepMemory {
-    role?: string;
-    room?: string;
-    sourceId?: string;
-    working?: boolean;
-  }
-
-  interface ProfilerMemory {
-    map: Record<string, { time: number; calls: number }>;
-    totalTime: number;
-    enabledTick: number;
-    disableTick: number | false;
-    type: string;
-    filter?: string;
-  }
-
-  // Syntax for adding proprties to `global` (ex "global.log")
-  namespace NodeJS {
-    interface Global {
-      log: any;
-      Memory?: Memory;
-      profiler?: Profiler;
-      envManager: typeof envManager
-      logger: typeof logger
-    }
-  }
-}
+import { RoomManager } from "roomManager/roomManager";
 
 const stats = new Stats();
 const memoryService = new MemoryService()
